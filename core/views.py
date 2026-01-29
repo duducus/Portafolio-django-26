5311b4308474c49c7f2fec59682d740a40d3cd36
duducus <jorge.ceron.gil@gmail.com>
2026-01-29 15:45:29 -0600

    ampliando la app core

--- core/views.py
+++ core/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1> Mi web personal </h1>
+<ul>
+	<li><a href="/">Portada</a></li>
+	<li><a href="/about-me/">Acerca de</a></li>
+</ul>
+
+"""
+
 # Create your views here.
 def home(request):
-	return HttpResponse("<h1>Mi web personal</h1><h2>Portada</h2>")
+	return HttpResponse( html_base + """
+		<h2>Portada</h2>
+		<p>Esto es la portada</p>
+	""")
+
+def about(request):
+	return HttpResponse("""
+		<h2>Acerca de mi</h2>
+		<p>Me llamo Cerón y soy el mejor en lo que hago</p>
+
+		""")
