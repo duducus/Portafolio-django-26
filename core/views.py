e82180db3597a743f20005e900a9ce4e0169afe8
duducus <jorge.ceron.gil@gmail.com>
2026-01-29 15:58:10 -0600

    agregando otras funcionalidades

--- core/views.py
+++ core/views.py
@@ -4,7 +4,10 @@ html_base = """
 <h1> Mi web personal </h1>
 <ul>
 	<li><a href="/">Portada</a></li>
+	<li><a href="/portfolio/">Portafolio</a></li>
+	<li><a href="/contact/">Contacto</a></li>
 	<li><a href="/about-me/">Acerca de</a></li>
+
 </ul>
 
 """
@@ -16,9 +19,23 @@ def home(request):
 		<p>Esto es la portada</p>
 	""")
 
-def about(request):
-	return HttpResponse("""
+def portfolio(request):
+	return HttpResponse(html_base + """
 		<h2>Acerca de mi</h2>
+		<p>Mira mi trabajo, para que te animes</p>
+
+		""")
+
+def contact(request):
+	return HttpResponse(html_base + """
+		<h2>Contacto</h2>
+		<p>Llamame para que haga tus bases de datos</p>
+
+		""")		
+
+def about(request):
+	return HttpResponse(html_base + """
+		<h2>Sobre mi</h2>
 		<p>Me llamo Cerón y soy el mejor en lo que hago</p>
 
 		""")
