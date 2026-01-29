47b365e0646ebcb14d9bc52c1249ffe7a53861d1
duducus <jorge.ceron.gil@gmail.com>
2026-01-29 16:14:51 -0600

    agregando todavia mas funcionalidades perronas

--- core/views.py
+++ core/views.py
@@ -1,41 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1> Mi web personal </h1>
-<ul>
-	<li><a href="/">Portada</a></li>
-	<li><a href="/portfolio/">Portafolio</a></li>
-	<li><a href="/contact/">Contacto</a></li>
-	<li><a href="/about-me/">Acerca de</a></li>
-
-</ul>
-
-"""
 
 # Create your views here.
 def home(request):
-	return HttpResponse( html_base + """
-		<h2>Portada</h2>
-		<p>Esto es la portada</p>
-	""")
+	return render(request, "core/home.html")
 
 def portfolio(request):
-	return HttpResponse(html_base + """
-		<h2>Acerca de mi</h2>
-		<p>Mira mi trabajo, para que te animes</p>
-
-		""")
+	return render(request, "core/portfolio.html")
 
 def contact(request):
-	return HttpResponse(html_base + """
-		<h2>Contacto</h2>
-		<p>Llamame para que haga tus bases de datos</p>
-
-		""")		
+	return render(request, "core/contact.html")
 
 def about(request):
-	return HttpResponse(html_base + """
-		<h2>Sobre mi</h2>
-		<p>Me llamo Cerón y soy el mejor en lo que hago</p>
-
-		""")
+	return render(request, "core/about-me.html")
